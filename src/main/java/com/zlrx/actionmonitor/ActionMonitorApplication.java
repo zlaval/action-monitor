e7cb3b0b0ee52288a9199ad74f6f44b16f83d39b
zalan <zlaval@gmail.com>
2019-03-28 19:50:48 +0100

    initial commit

---
+++ src/main/java/com/zlrx/actionmonitor/ActionMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.zlrx.actionmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActionMonitorApplication.class, args);
+    }
+
+}
